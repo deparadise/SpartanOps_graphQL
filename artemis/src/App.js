e7de34e8971cda825695ab367fc1c3e44835648a
David Pineda <david.pineda@deparadise.com>
2019-10-19 07:49:45 -0700

    User Prettier vscode extension

--- artemis/src/App.js
+++ artemis/src/App.js
@@ -2,28 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//TODO: - Start router
+//TODO: -> Start router
 //TODO: - ADD main navigation
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<main className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</main>
+		</div>
+	);
 }
 
 export default App;
