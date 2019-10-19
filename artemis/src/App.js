8bb204d009d99749081ad73c76b60c0117b264f1
David Pineda <david.pineda@deparadise.com>
2019-10-19 10:05:12 -0700

    Setup/starting project (#1)
    * Start project package and gitignore
    
    * Revert "Start project package and gitignore"
    
    This reverts commit 892de4474cffeebcf7aa708c01ecce58351ed197.
    
    * Terminal: npx create-react-app artemis
    
    * Start plan for router and nav
    
    * Add a root package and prettier
    
    * User Prettier vscode extension
    
    * Add and start using Reach Router

---
+++ artemis/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Router } from '@reach/router';
+import logo from './logo.svg';
+import './App.css';
+
+import RouteOne from './Route-one';
+
+//TODO: -> Start router
+//TODO: - ADD main navigation
+
+function App() {
+	return (
+		<div className="App">
+			<header>
+				<Router>
+					<RouteOne path="/one" />
+				</Router>
+			</header>
+			<main className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</main>
+		</div>
+	);
+}
+
+export default App;
