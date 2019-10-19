ae9ba15d439ac4469bf39b793ab00f055cbaf15a
David Pineda <david.pineda@deparadise.com>
2019-10-19 11:13:47 -0700

    Import new routes  and utilize Reach Router

--- artemis/src/App.js
+++ artemis/src/App.js
@@ -3,17 +3,22 @@ import { Router } from '@reach/router';
 import logo from './logo.svg';
 import './App.css';
 
-import RouteOne from './Route-one';
-
-//TODO: -> Start router
-//TODO: - ADD main navigation
+import Artemis from './view/console';
+import Engagements from './view/engagements';
+import Personnel from './view/personnel';
+import Inventory from './view/inventory';
+import BattleLog from './view/battle-log';
 
 function App() {
 	return (
 		<div className="App">
 			<header>
 				<Router>
-					<RouteOne path="/one" />
+					<Artemis path="/" />
+					<Engagements path="/engagement" />
+					<Personnel path="/personnel" />
+					<Inventory path="/inventory" />
+					<BattleLog path="/battle-log" />
 				</Router>
 			</header>
 			<main className="App-header">
