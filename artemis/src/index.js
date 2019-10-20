1842ea1fb36c79f6c5b5289bd0183407d8ec3a73
David Pineda <david.pineda@deparadise.com>
2019-10-20 13:26:57 -0700

    Provide redux store to ARTEMIS app

--- artemis/src/index.js
+++ artemis/src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import store from "./store";
 
+import App from "./App";
+
+import "./index.css";
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
