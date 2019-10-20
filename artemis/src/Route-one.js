71a9ca995400d4634b209ced38585479873a5ef6
David Pineda <david.pineda@deparadise.com>
2019-10-20 10:35:15 -0700

    Feature/routing (#2)
    * Start main view routes
    
    * Import new routes  and utilize Reach Router
    
    * Add Reach Router navigation

--- artemis/src/Route-one.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function RouteOne() {
-	return <h1>Route One</h1>;
-}
-
-export default RouteOne;
---
+++ artemis/src/view/battle-log.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BattleLog() {
+	return <h1>BATTLE LOG</h1>;
+}
+
+export default BattleLog;
