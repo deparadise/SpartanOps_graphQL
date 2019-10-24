dfd3e68a59c3063af1eef1b3630802008024fa42
David Pineda <david.pineda@deparadise.com>
2019-10-23 22:21:07 -0700

    Start createStore() index

--- artemis/src/store/index.js
+++ artemis/src/store/index.js
@@ -1,9 +1,23 @@
-import { createStore } from "redux";
+// STARTING STORE INDEX
+import { createStore, compose, applyMiddleware } from 'redux';
 
-// import reducers from '../reducers'; // TODO: reducers TBD
-import initialState from "./initial-state";
+// COMBINED REDUCER INDEX >>
+import combinedReducers from '../reducers';
+// COMPOSED STATE MODEL TREE >>
+import initialState from './initial-state';
+
+// START TERTIARY MIDDLE WARE & ENHANCERS >>
+const middleware = [];
+const enhancer = [];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+console.log('>> STORE WILL USE:', {
+	combined_reducers: combinedReducers,
+	initial_state_tree: initialState
+});
 
 export default createStore(
-	() => {}, // TODO: combineReducers imp TBD
-	initialState
+	combinedReducers,
+	initialState,
+	composeEnhancers(applyMiddleware(...middleware), ...enhancer)
 );
