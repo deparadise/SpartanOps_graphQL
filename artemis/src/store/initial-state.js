f1feb10eeb656ffb8431ab8b6a75cb95f47487cc
David Pineda <david.pineda@deparadise.com>
2019-10-23 22:59:09 -0700

    Start initial state tree index that exports defaultFor<node> objects

--- artemis/src/store/initial-state.js
+++ artemis/src/store/initial-state.js
@@ -1,26 +1,35 @@
+// STARTING INITIAL STATE INDEX
 // import uniqueId from 'lodash/uniqueId';
 
-// >>> Temp initilal model payloads go here <<<
-
-// ARTEMIS STATE MODEL
-export default {
-	console: {
+// DECLARE DEFAULT STATE NODES AS EXPORTS >>
+export const defaultForConsole = () => {
+	return {
+		wake: false,
 		onBoardStatus: {},
 		preferences: {}
-	},
-	engagementIndex: {},
-	battleLogIndex: {},
-	personnelIndex: {},
-	inventoryIndex: {},
-	models: {
-		fleet: [],
-		personnel: [],
-		inventory: {
-			transport: [],
-			airSupport: [],
-			groundSupport: [],
-			armament: {},
-			supplies: {}
-		}
-	}
+	};
 };
+
+// COMPOSE DEFAULT STATE TREE FROM DEFAULT EXPORT NODES >>
+// NOTE: This will be the shape of the store
+const defaultStateTree = {
+	consoleState: defaultForConsole()
+	// engagementIndex: {},
+	// battleLogIndex: {},
+	// personnelIndex: {},
+	// inventoryIndex: {},
+	// models: {
+	// 	fleet: [],
+	// 	personnel: [],
+	// 	inventory: {
+	// 		transport: [],
+	// 		airSupport: [],
+	// 		groundSupport: [],
+	// 		armament: {},
+	// 		supplies: {}
+	// 	}
+	// }
+};
+
+console.log('>> STATE INDEX COMPOSED INITIAL STATE TREE:', defaultStateTree);
+export default defaultStateTree;
